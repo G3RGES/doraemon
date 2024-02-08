affea38acb5303ab0836cf35530958cdf84d2298
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-08 13:33:48 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,57 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 
+// icons
+import { GiAtomicSlashes } from "react-icons/gi";
+import { FaSearch } from "react-icons/fa";
+import { MdArrowDropDown } from "react-icons/md";
+
+const MenuLinks = [
+  {
+    name: "Episodes",
+    link: "#/",
+  },
+  {
+    name: "Store",
+    link: "#/store",
+  },
+  {
+    name: "Toys",
+    link: "#/toys",
+  },
+  {
+    name: "3D videos",
+    link: "#/videos",
+  },
+];
+
 const Navbar = () => {
   return (
     <div className="">
-      <div className="container">navbar</div>
+      <div className="container">
+        <div className="flex items-center justify-between">
+          {/* logo section */}
+          <div className="flex flex-row items-center gap-4">
+            <GiAtomicSlashes className="text-3xl text-primary" />
+            <div className="flex flex-col text-2xl font-bold text-gray-600 leading-5">
+              <span className="">Dora</span>
+              <span className="">emon</span>
+            </div>
+          </div>
+
+          {/* links section */}
+          <div className="hidden md:block">
+            <ul className="">
+              {MenuLinks.map((link, idx) => (
+                <li key={idx} className=""></li>
+              ))}
+            </ul>
+          </div>
+
+          {/* search section */}
+          <div className=""></div>
+        </div>
+      </div>
     </div>
   );
 };
