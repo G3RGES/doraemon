1cb8f69ebb882a54023ff0008902ad5140fd98fc
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-08 13:05:49 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 /* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useEffect } from "react";
+
+// aos
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+// components
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return <div className="">App</div>;
+  useEffect(() => {
+    // Initialize AOS
+    AOS.init({
+      duration: 600,
+      easing: "ease-in-sine",
+      offset: 100,
+    });
+  }, []);
+
+  return (
+    <div className="overflow-hidden">
+      <Navbar />
+    </div>
+  );
 };
 
 export default App;
