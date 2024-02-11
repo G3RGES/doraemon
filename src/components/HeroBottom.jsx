c030f39c7e9ed7b0ac694bcf433dd69eba062397
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-11 14:24:19 +0200

    Update HeroBottom.jsx

--- src/components/HeroBottom.jsx
+++ src/components/HeroBottom.jsx
@@ -3,6 +3,7 @@ import React from "react";
 
 // icons
 import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
+
 import { FaArrowDown } from "react-icons/fa";
 
 const HeroBottom = () => {
@@ -22,11 +23,45 @@ const HeroBottom = () => {
       </div>
 
       {/* bottom section */}
-      <div className="">
-        <div className="">
+      <div className=" absolute bottom-0 right-0 w-full">
+        <div className="grid sm:grid-cols-3 container">
           {/* first column */}
+
+          <div className="hidden sm:block">
+            <div className="font-semibold">
+              <p className="text-gray-500 ">Sohag</p>
+              <p className="text-sm text-primary">Egypt</p>
+            </div>
+            <div className="font-semibold">
+              <p className="text-gray-500 ">11Feb, 2024</p>
+            </div>
+            <div></div>
+          </div>
+
           {/* second column */}
+          <div className="center">
+            <span className="hidden sm:block">
+              <FaArrowDown />
+            </span>
+          </div>
+
           {/* third column */}
+          <div className="flex justify-center sm:justify-end mb-4">
+            <div className="flex flex-row sm:flex-col gap-5">
+              <div
+                className="bg-white rounded-full p-2 
+              shadow-md hover:scale-110 duration-200"
+              >
+                <IoIosArrowBack className="text-primary" />
+              </div>
+              <div
+                className="bg-primary rounded-full p-2 
+              shadow-md hover:scale-110 duration-200"
+              >
+                <IoIosArrowForward className="text-white" />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
