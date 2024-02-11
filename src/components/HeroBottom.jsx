7116df35317e9d1d6992188e917cb91ac471e2c3
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-11 13:42:27 +0200

    Update HeroBottom.jsx

--- src/components/HeroBottom.jsx
+++ src/components/HeroBottom.jsx
@@ -2,7 +2,21 @@
 import React from "react";
 
 const HeroBottom = () => {
-  return <div>HeroBottom</div>;
+  return (
+    <>
+      <div
+        className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+      z-0 w-full"
+      >
+        <h1
+          className="text-6xl sm:text-[140px] md:text-[190px] font-bold
+        text-center uppercase text-black "
+        >
+          Doraemon
+        </h1>
+      </div>
+    </>
+  );
 };
 
 export default HeroBottom;
