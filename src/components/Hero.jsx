1d436a12d95199053d29ac602ba03b79178352cf
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-11 13:42:24 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
   return (
     <>
       <div className="min-h-[700px] center relative">
-        <div className="container mt-12 sm:mt-0 pb-20 sm:pb-0 ">
+        <div className="container mt-12 sm:mt-0 pb-20 sm:pb-0 relative z-20">
           <div className="grid grid-cols-1 sm:grid-cols-3 gap-6 sm:gap-0">
             {/* text content section */}
             <div
