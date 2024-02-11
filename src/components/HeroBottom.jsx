ccc2aae552d64d71a22ea51464ff82dc03544b4c
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2024-02-11 13:44:18 +0200

    Update HeroBottom.jsx

--- src/components/HeroBottom.jsx
+++ src/components/HeroBottom.jsx
@@ -4,13 +4,14 @@ import React from "react";
 const HeroBottom = () => {
   return (
     <>
+      {/* doraemon text background */}
       <div
         className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
       z-0 w-full"
       >
         <h1
           className="text-6xl sm:text-[140px] md:text-[190px] font-bold
-        text-center uppercase text-black "
+        text-center uppercase text-gray-300/25 "
         >
           Doraemon
         </h1>
